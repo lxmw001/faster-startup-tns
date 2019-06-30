76a1aaeceb1d0fa963f2b60c0272418a19ac5ae3
Luis Gonzalez <lx.mw001@gmail.com>
2019-06-30 09:17:44 -0500

    Styled action bar

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: "ft",
     moduleId: module.id,
-    templateUrl: "./feature.component.html"
+    templateUrl: "./feature.component.html",
+    styleUrls: ['./feature.component.css']
 })
 export class FeatureComponent {
     constructor() { }
