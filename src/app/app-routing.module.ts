cf5506611916cba0e70f0f13abcf962ec48dedcb
Luis Gonzalez <lx.mw001@gmail.com>
2019-06-30 01:59:42 -0500

    Removing unused code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { Routes } from "@angular/router";
 
 const routes: Routes = [
     { path: "", redirectTo: "/items", pathMatch: "full" },
-    // { path: "items", component: ItemsComponent },
     { path: "items", loadChildren: "./item/items.module#ItemsModule" },
-    // { path: "item/:id", component: ItemDetailComponent },
      { path: "feature", loadChildren: "./feature/feature.module#FeatureModule" },
 ];
 
